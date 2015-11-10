13070acde3f25b2616bd1f52fb68b7379e8192c6
Eduardo Thomaz de Carvalho <2641743+talkedu@users.noreply.github.com>
2015-11-10 20:31:22 +0000

    asdsad

--- ComandaRestaurante/app/src/main/java/br/com/ffit/comanda/service/EstabelecimentoService.java
+++ ComandaRestaurante/app/src/main/java/br/com/ffit/comanda/service/EstabelecimentoService.java
@@ -1,33 +1,20 @@
-package br.com.ffit.comanda.service;
+package br.com.ffit.comanda.rest;
 
+import org.androidannotations.annotations.rest.Post;
+import org.androidannotations.annotations.rest.Rest;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
-import org.androidannotations.annotations.Background;
-import org.androidannotations.annotations.EBean;
-import org.androidannotations.annotations.rest.RestService;
-
-import br.com.ffit.comanda.activity.LoginRestauranteActivity;
-import br.com.ffit.comanda.rest.RestClient;
 import br.com.ffit.comanda.to.JSONResponse;
 import br.com.ffit.comanda.to.LoginTO;
 import br.com.ffit.comanda.to.ProdutoTO;
 
-@EBean
-public class EstabelecimentoService {
+@Rest(rootUrl = "http://192.168.100.84:8080/comanda-server/estabelecimento", converters = {MappingJackson2HttpMessageConverter.class})
+public interface EstabelecimentoService {
 
-    @RestService
-    RestClient restClient;
+    @Post("/inserirProduto")
+    void inserirProduto(ProdutoTO produtoTO);
 
-    @Background
-    public void inserirProduto(ProdutoTO produtoTO) {
-        restClient.inserirProduto(produtoTO);
-    }
-
-    @Background
-    public void fazerLogin(LoginTO loginTO, LoginRestauranteActivity activity) {
-        JSONResponse jsonResponse = restClient.fazerLogin(loginTO);
-        activity.setJSONResponse(jsonResponse);
-    }
-
-
+    @Post("/fazerLogin")
+    JSONResponse fazerLogin(LoginTO loginTO);
 
 }
