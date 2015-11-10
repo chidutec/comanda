d923fe028852cb9476991a48141378edd71acbaf
ethomaz <ethomaz@f9283171-faac-4340-b2dc-cbbbdc3c1104>
2015-11-10 20:31:22 +0000

    asdsad

--- trunk/comanda/ComandaRestaurante/app/src/main/java/br/com/ffit/comanda/activity/CadastroProdutoActivity.java
+++ trunk/comanda/ComandaRestaurante/app/src/main/java/br/com/ffit/comanda/activity/CadastroProdutoActivity.java
@@ -3,21 +3,21 @@ package br.com.ffit.comanda.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 
-import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.rest.RestService;
 
 import java.math.BigDecimal;
 
-import br.com.ffit.comanda.service.EstabelecimentoService;
+import br.com.ffit.comanda.rest.EstabelecimentoService;
 import br.com.ffit.comanda.to.ProdutoTO;
 import ffit.com.br.comanda.R;
 
 @EActivity(R.layout.activity_cadastro_produto)
 public class CadastroProdutoActivity extends AppCompatActivity {
 
-        @ViewById(R.id.inputNome)
+       @ViewById(R.id.inputNome)
         EditText inputNome;
 
         @ViewById(R.id.inputDesc)
@@ -26,7 +26,7 @@ public class CadastroProdutoActivity extends AppCompatActivity {
         @ViewById(R.id.inputPreco)
         EditText inputPreco;
 
-        @Bean
+        @RestService
         EstabelecimentoService estabelecimentoService;
 
         @Click
--- trunk/comanda/ComandaRestaurante/app/src/main/java/br/com/ffit/comanda/activity/LoginRestauranteActivity.java
+++ trunk/comanda/ComandaRestaurante/app/src/main/java/br/com/ffit/comanda/activity/LoginRestauranteActivity.java
@@ -1,27 +1,28 @@
 package br.com.ffit.comanda.activity;
 
+import android.app.Activity;
 import android.support.annotation.UiThread;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import org.androidannotations.annotations.Bean;
+import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 import org.androidannotations.annotations.rest.RestService;
 
-import br.com.ffit.comanda.rest.RestClient;
-import br.com.ffit.comanda.service.EstabelecimentoService;
+import br.com.ffit.comanda.rest.EstabelecimentoService;
 import br.com.ffit.comanda.to.JSONResponse;
 import br.com.ffit.comanda.to.LoginTO;
 import ffit.com.br.comanda.R;
 
 @EActivity(R.layout.activity_login)
-public class LoginRestauranteActivity extends AppCompatActivity {
+public class LoginRestauranteActivity extends Activity {
 
 
-    private JSONResponse jsonResponse;
+    @RestService
+    EstabelecimentoService estabelecimentoService;
 
     @ViewById(R.id.inputEmail)
     EditText inputEmail;
@@ -29,14 +30,9 @@ public class LoginRestauranteActivity extends AppCompatActivity {
     @ViewById(R.id.inputSenha)
     EditText inputSenha;
 
-    @ViewById(R.id.txtMessageLogin)
-    TextView txtMessageLogin;
+    @ViewById(R.id.viewMessageLogin)
+    TextView viewMessageLogin;
 
-    @RestService
-    RestClient restClient;
-
-    @Bean
-    EstabelecimentoService estabelecimentoService;
 
     @Click
     public void btnLogin() {
@@ -44,29 +40,36 @@ public class LoginRestauranteActivity extends AppCompatActivity {
         String login = inputEmail.getText().toString();
         String senha = inputSenha.getText().toString();
 
-        if (login == null && senha != null) {
-            txtMessageLogin.setText("Preencha o Login");
-        } else if(login != null && senha == null) {
-            txtMessageLogin.setText("Preencha a Senha");
-        } else if(login != null && senha == null) {
-            txtMessageLogin.setText("Preencha login e senha");
+        if (login.isEmpty() && !senha.isEmpty()) {
+            Toast.makeText(this, "Preencha o Login", Toast.LENGTH_SHORT).show();
+        } else if(!login.isEmpty() && senha.isEmpty()) {
+            Toast.makeText(this, "Preencha a Senha", Toast.LENGTH_SHORT).show();
+        } else if(login.isEmpty() && senha.isEmpty()) {
+            Toast.makeText(this, "Preencha login e senha", Toast.LENGTH_SHORT).show();
+
         } else {
 
             LoginTO loginTO = new LoginTO();
             loginTO.setLogin(login);
             loginTO.setSenha(senha);
-            estabelecimentoService.fazerLogin(loginTO, this);
-
+            fazerLogin(loginTO);
         }
     }
 
+    @Background
+    public void fazerLogin(LoginTO loginTO) {
+        JSONResponse jsonResponse = estabelecimentoService.fazerLogin(loginTO);
+        callBackFazerLogin(jsonResponse);
+    }
 
     @UiThread
-    public void setJSONResponse(JSONResponse jsonResponse) {
+    public void callBackFazerLogin(JSONResponse jsonResponse) {
         if(jsonResponse.getSuccess()) {
-            //Lancar activity
+            //Proxima activity
         } else {
-            txtMessageLogin.setText("Login/Senha incorreto");
+            //Toast.makeText(getApplicationContext(), jsonResponse.getErrorMessage(), Toast.LENGTH_SHORT).show();
+            viewMessageLogin.setText(jsonResponse.getErrorMessage());
         }
     }
+
 }
