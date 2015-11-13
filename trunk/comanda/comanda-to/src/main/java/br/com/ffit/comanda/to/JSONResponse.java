fd7f5fb97042558d6ed3ee99ec69b3fa1cfa9211
ethomaz <ethomaz@f9283171-faac-4340-b2dc-cbbbdc3c1104>
2015-11-13 15:09:39 +0000

    -importando comanda-to

---
+++ trunk/comanda/comanda-to/src/main/java/br/com/ffit/comanda/to/JSONResponse.java
@@ -0,0 +1,32 @@
+package br.com.ffit.comanda.to;
+
+public class JSONResponse<T> {
+
+    Boolean success;
+    String message;
+    T obj;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public T getObj() {
+        return obj;
+    }
+
+    public void setObj(T obj) {
+        this.obj = obj;
+    }
+}
