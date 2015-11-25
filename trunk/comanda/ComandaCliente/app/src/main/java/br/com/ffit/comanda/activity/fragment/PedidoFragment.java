d7151b0cbdbf1d13779513264f1bb1a47c1f86b4
ethomaz <ethomaz@f9283171-faac-4340-b2dc-cbbbdc3c1104>
2015-11-25 22:00:28 +0000

    -ajustando tabs da conta fragment

--- trunk/comanda/ComandaCliente/app/src/main/java/br/com/ffit/comanda/activity/fragment/PedidoFragment.java
+++ trunk/comanda/ComandaCliente/app/src/main/java/br/com/ffit/comanda/activity/fragment/PedidoFragment.java
@@ -1,7 +1,7 @@
 package br.com.ffit.comanda.activity.fragment;
 
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.widget.TextView;
 
 import org.androidannotations.annotations.EFragment;
