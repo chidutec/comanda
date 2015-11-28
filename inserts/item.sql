cbbac46fa39fd6a8da17ead5cb1ca689723d0eb1
franciscocgfreire <18204733+franciscocgfreire@users.noreply.github.com>
2015-11-28 03:21:59 +0000

    Inserts para popular banco. v1

---
+++ inserts/item.sql
@@ -0,0 +1,6 @@
+USE comanda;
+
+insert into comanda.item(id,quantidade,conta_id,produto_id) values (1,2,4,2);
+insert into comanda.item(id,quantidade,conta_id,produto_id) values (2,2,4,2);
+insert into comanda.item(id,quantidade,conta_id,produto_id) values (3,1,4,7);
+insert into comanda.item(id,quantidade,conta_id,produto_id) values (4,28,4,8);
---
+++ inserts/usuario_item.sql
@@ -0,0 +1,8 @@
+USE comanda;
+
+insert into comanda.usuario_item(id,Quantidade,item_id,usuario_id) values (1,5.00,4,1);
+insert into comanda.usuario_item(id,Quantidade,item_id,usuario_id) values (2,5.00,4,2);
+insert into comanda.usuario_item(id,Quantidade,item_id,usuario_id) values (3,5.00,3,2);
+insert into comanda.usuario_item(id,Quantidade,item_id,usuario_id) values (4,5.00,3,1);
+insert into comanda.usuario_item(id,Quantidade,item_id,usuario_id) values (5,1.00,2,2);
+insert into comanda.usuario_item(id,Quantidade,item_id,usuario_id) values (6,1.00,1,1);
