165c1246be21f645925c331613dd9c12c5524a40
ethomaz <ethomaz@f9283171-faac-4340-b2dc-cbbbdc3c1104>
2015-11-29 08:09:07 +0000

    -adicionado metodo de fechar conta e tela de listar conta

--- trunk/comanda/comanda-to/src/main/java/br/com/ffit/comanda/to/ContaTO.java
+++ trunk/comanda/comanda-to/src/main/java/br/com/ffit/comanda/to/ContaTO.java
@@ -2,14 +2,18 @@ package br.com.ffit.comanda.to;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.Date;
 
 public class ContaTO implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
+	private String nomeRestaurante;
+	private Date dataAberturaUsuario;
+	private BigDecimal valorParcial;
 	private BigDecimal valorTotal;
-
+	
 	public Long getId() {
 		return id;
 	}
@@ -18,11 +22,35 @@ public class ContaTO implements Serializable{
 		this.id = id;
 	}
 
+	public String getNomeRestaurante() {
+		return nomeRestaurante;
+	}
+
+	public void setNomeRestaurante(String nomeRestaurante) {
+		this.nomeRestaurante = nomeRestaurante;
+	}
+
 	public BigDecimal getValorTotal() {
 		return valorTotal;
 	}
 
 	public void setValorTotal(BigDecimal valorTotal) {
 		this.valorTotal = valorTotal;
+	}
+
+	public Date getDataAberturaUsuario() {
+		return dataAberturaUsuario;
+	}
+
+	public void setDataAberturaUsuario(Date dataAberturaUsuario) {
+		this.dataAberturaUsuario = dataAberturaUsuario;
+	}
+
+	public BigDecimal getValorParcial() {
+		return valorParcial;
+	}
+
+	public void setValorParcial(BigDecimal valorParcial) {
+		this.valorParcial = valorParcial;
 	}
 }
---
+++ trunk/comanda/comanda-to/src/main/java/br/com/ffit/comanda/to/FecharContaTO.java
@@ -0,0 +1,24 @@
+package br.com.ffit.comanda.to;
+
+public class FecharContaTO {
+
+	private Long idUsuario;
+	private Long idConta;
+
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public Long getIdConta() {
+		return idConta;
+	}
+
+	public void setIdConta(Long idConta) {
+		this.idConta = idConta;
+	}
+
+}
